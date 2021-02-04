1f560889e0038631c8687126b07da531947fbe5c
David Lazaro <alu0100770032@ull.edu.es>
2021-02-04 10:30:04 +0000

    Falla la instancia de matriz

--- spec/dia_spec.rb
+++ spec/dia_spec.rb
@@ -1,9 +1,6 @@
 RSpec.describe Dia do
-  it "has a version number" do
-    expect(Dia::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(true).to eq(true)
-  end
+  @prueba = dia.new()
+  it "se puede instanciar la matriz" do
+    expect(@prueba).not_to eq (nil)
+  end 
 end

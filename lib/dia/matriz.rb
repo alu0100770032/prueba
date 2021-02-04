04e6ef67f9af1158ad6791b061811d2e4fd164c1
David Lazaro <alu0100770032@ull.edu.es>
2021-02-04 12:43:47 +0000

    Se añaden y se muestran personas correctamente

--- lib/dia/matriz.rb
+++ lib/dia/matriz.rb
@@ -18,9 +18,21 @@ class Matriz
     @matriz.to_s
   end
   def to_s_personas
-   
+    output = "Producto:\n"
+    @personas.each do |persona|
+        output << persona + "\n"
+    end
+    output
   end
   def persona(nombre, options = {})
-   
+    persona = ""
+    persona << " [#{options[:peso]}] " if options[:peso]
+    persona << "" + nombre + " : "
+    persona << "#{options[:altura]} " if options[:ojos]
+    persona << "#{options[:ojos]} " if options[:ojos]
+    persona << "#{options[:pelo]} " if options[:ojos]
+    persona << "#{options[:pie]}" if options[:ojos]
+    
+    @personas << persona
 end
 end

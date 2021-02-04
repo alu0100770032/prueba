eb1f3d2424191e917e35d673cef6559d610c1bb4
David Lazaro <alu0100770032@ull.edu.es>
2021-02-04 10:24:20 +0000

    Punto de partida

---
+++ lib/dia.rb
@@ -0,0 +1,4 @@
+require "dia/version"
+
+module Dia
+end
---
+++ spec/dia_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Dia do
+  it "has a version number" do
+    expect(Dia::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(true).to eq(true)
+  end
+end
